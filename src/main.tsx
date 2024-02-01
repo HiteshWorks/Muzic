eee9a66203ffb9f7a08861de4e4e432d9a3fb1a2
HiteshWorks <hiteshpandey442@gmail.com>
2024-02-01 09:26:27 +0530

    few bugs fixed

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import Playsong from './playsong.tsx'
+import Playsong from './Playsong.tsx'
 import {Route ,Routes, BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
